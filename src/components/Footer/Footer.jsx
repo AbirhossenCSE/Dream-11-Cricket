dceb00ae00e941cba33dc2d99aa7d1c6f94c20cc
Abir Hossen <abir.cse4@gmail.com>
2024-10-27 12:17:27 +0600

    footer

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,46 @@
+import footerb from '../../assets/image/logo-footer.png'
+import './Footer.css'
+const Footer = () => {
+    return (
+        <div className="mt-36">
+            <div className="bg-[#06091A] text-white">
+                <div className='relative -top-16 bg-white w-8/12 mx-auto rounded-xl text-black'>
+                    <div className='bg2 w-full flex flex-col space-y-4 justify-center items-center'>
+                        <h2 className='text-2xl font-bold'>Subscribe to our Newsletter</h2>
+                        <p className='text-gray-400 items-center'>Get the latest updates and news right in your inbox!</p>
+                        <div className='p-4'>
+                            <input className="rounded-xl p-2" type="text" placeholder="Enter Your Emai" />
+                            <button className="bg-white text-black rounded-xl ml-1 p-2">Subscribe</button>
+                        </div>
+                    </div>
+                </div>
+                <div>
+                    <img className='mx-auto' src={footerb} alt="" />
+                </div>
+                <div className="w-10/12 mx-auto flex space-y-5 p-8 gap-5">
+                    <div className="flex-1">
+                        <h3 className="mb-2 font-bold text-white">About Us</h3>
+                        <p className="text-gray-400">We are a passionate team dedicated to providing the best services to our customers.</p>
+                    </div>
+                    <nav className="flex-1">
+                        <h6 className="mb-2 font-bold text-white">Quick Links</h6>
+                        <ul className='gap-6 text-gray-400'>
+                            <li>Home</li>
+                            <li>Picture</li>
+                            <li>Teams</li>
+                            <li>Schedules</li>
+                        </ul>
+                    </nav>
+                    <div className="flex-1 space-y-2">
+                        <h3 className="mb-2 font-bold text-white">Subscribe</h3>
+                        <p className="text-gray-400">Subscribe to our newsletter for the latest updates.</p>
+                        <input className="rounded-l-xl p-2" type="text" placeholder="Enter Your Emai" />
+                        <button className="bg-[#ff00ff] text-black rounded-r-xl p-2">Subscribe</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Footer;
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -18,11 +18,13 @@ const Header = () => {
                     <button className='border p-2'>Coin</button>
                 </div>
             </div>
-            <div className='bg w-10/12 mx-auto p-8 rounded-xl flex flex-col space-y-4 justify-center items-center'>
-                <img className='' src={banner} alt="" />
-                <h2>Assemble Your Ultimate Dream 11 Cricket Team</h2>
-                <p>Beyond Boundaries Beyond Limits</p>
-                <button className='bg-yellow-400 p-3 border-2 rounded-xl'>Claim Free Coin</button>
+            <div className='bg-black w-10/12 mx-auto rounded-xl'>
+                <div className='bg w-12/12 mx-auto p-8 rounded-xl flex flex-col space-y-4 justify-center items-center'>
+                    <img className='' src={banner} alt="" />
+                    <h2 className='text-white text-2xl font-bold'>Assemble Your Ultimate Dream 11 Cricket Team</h2>
+                    <p className='text-gray-400 '>Beyond Boundaries Beyond Limits</p>
+                    <button className='bg-yellow-400 p-3 border-2 rounded-xl'>Claim Free Coin</button>
+                </div>
             </div>
         </div>
     );
